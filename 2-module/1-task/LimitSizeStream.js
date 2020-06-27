66270a311c7c451e5692dcfa56694959baaeb3d5
achat <achat@custis.ru>
2020-06-27 19:13:22 +0300

    add solution for 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -2,12 +2,24 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #totalLength = 0;
+  #limit = 0;
+
   constructor(options) {
-    super(options);
+    super(Object.assign(options, {decodeStrings: true}));
+    this.#limit = options.limit;
+    if (!options.objectMode) {
+      this.setEncoding('utf8');
+    }
   }
 
   _transform(chunk, encoding, callback) {
-
+    if (this.#totalLength + this._writableState.length <= this.#limit) {
+      this.#totalLength += this._writableState.length;
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError(), null);
+    }
   }
 }
 
