420e1c353661e2141114642a953ff038da05d9c7
achat <achat@custis.ru>
2020-07-04 23:46:07 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,31 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let responses = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  responses.push(ctx.response);
+  await new Promise((resolve) => {
+    let interval = setInterval(() => {
+      if (ctx.res.finished) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 100);
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  let message = ctx.request.body.message;
+  if (message) {
+    responses.forEach(response => {
+      response.status = 200;
+      response.res.end(message);
+    });
+    responses = [];
+  }
+  ctx.response.status = 200;
+  ctx.res.end('Ok');
 });
 
 app.use(router.routes());
