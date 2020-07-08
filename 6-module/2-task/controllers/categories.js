85d108c3b6d1e23103dbb99065ebe9a10a54c596
achat <achat@custis.ru>
2020-07-08 16:21:54 +0300

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,20 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  let arr = await Category.find({});
+  let docs = arr.map((model) => {
+    let doc = model.toJSON();
+    let subDocs = doc.subcategories.map((sub) => {
+      return {
+        id: sub._id,
+        title: sub.title
+      }
+    });
+      return {
+      id: doc._id,
+      title: doc.title,
+      subcategories: subDocs
+  }});
+
+  ctx.body = {categories: docs};
 };
