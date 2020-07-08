8af5021cc7197d6332b15cb5fc784425c478ba5d
achat <achat@custis.ru>
2020-07-08 16:22:29 +0300

    add solution for 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,22 @@
+const url = require('url');
+const Product = require('../models/Product');
+
+function getTransformedArray(arr) {
+  return arr.map((model) => getDocFromProductModel(model));
+}
+
+function getDocFromProductModel(model) {
+  let doc = model.toJSON();
+  return {id: doc._id, ...doc};
+}
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const query = url.parse(ctx.req.url, true).query.query;
+  let arr = await Product
+  .find(
+    {$text: {$search: query}},
+    {score: {$meta: "textScore"}}
+  )
+  .sort({score: {$meta: 'textScore'}});
+  ctx.body = {products: getTransformedArray(arr)};
 };
