f1a8e806dee82ffd650b509374e50d3814669d9e
achat <achat@custis.ru>
2020-07-11 12:25:53 +0300

    add solution for 7-module 3-task

--- 7-module/3-task/config.js
+++ 7-module/3-task/config.js
@@ -11,24 +11,24 @@ module.exports = {
   },
   providers: {
     github: {
-      app_id: process.env.GITHUB_APP_ID || 'github_app_id',
-      app_secret: process.env.GITHUB_APP_SECRET || 'github_app_secret',
+      app_id: process.env.GITHUB_APP_ID || 'ea08e09854ed95cbe7ba',
+      app_secret: process.env.GITHUB_APP_SECRET || '454cd08121aa94167d7060f4ea01a0dbaa9f44bd',
       callback_uri: 'http://localhost:3000/oauth/github',
       options: {
         scope: ['user:email'],
       },
     },
     facebook: {
-      app_id: process.env.FACEBOOK_APP_ID || 'facebook_app_id',
-      app_secret: process.env.FACEBOOK_APP_SECRET || 'facebook_app_secret',
+      app_id: process.env.FACEBOOK_APP_ID || '271730287615321',
+      app_secret: process.env.FACEBOOK_APP_SECRET || '219af7001d81fe38f9e145b932305e31',
       callback_uri: 'http://localhost:3000/oauth/facebook',
       options: {
         scope: ['email'],
       },
     },
     vkontakte: {
-      app_id: process.env.VKONTAKTE_APP_ID || 'vkontakte_app_id',
-      app_secret: process.env.VKONTAKTE_APP_SECRET || 'vkontakte_app_secret',
+      app_id: process.env.VKONTAKTE_APP_ID || '7535642',
+      app_secret: process.env.VKONTAKTE_APP_SECRET || 'uuaZhyT8N0MWbhnxvleT',
       callback_uri: 'http://localhost:3000/oauth/vkontakte',
       options: {
         scope: ['email'],
--- 7-module/3-task/libs/mustBeAuthenticated.js
+++ 7-module/3-task/libs/mustBeAuthenticated.js
@@ -1,3 +1,8 @@
 module.exports = function mustBeAuthenticated(ctx, next) {
+  if (!ctx.user) {
+    let err = new Error('Пользователь не залогинен');
+    err.status = 401;
+    throw err;
+  }
   return next();
 };
