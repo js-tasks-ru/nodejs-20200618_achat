5b487b19e5f0afc905a84418442037c2847ba891
achat <achat@custis.ru>
2020-07-11 12:24:44 +0300

    add solution for 7-module 2-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,27 @@
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+  const User = require('../../models/User.js');
+
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  User.findOne({email: email}, null, null, async function (err, user) {
+    if (err) {
+      return done(err)
+    }
+    if (!user) {
+      user = new User({
+          "email": email,
+          "displayName": displayName
+        },
+      );
+      try {
+        await user.save();
+      } catch (e) {
+        return done(e);
+      }
+    }
+
+    return done(null, user)
+  })
 };
